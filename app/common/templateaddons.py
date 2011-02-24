c9874eb4de9f17965f2261dd932c8f55e9c09b03
Chris Hager <chager@myyearbook.com>
2011-02-23 23:15:42 -0500

    template addons boilerplate

--- app/common/templateaddons.py
+++ app/common/templateaddons.py
@@ -1,16 +1,25 @@
 from google.appengine.ext import webapp
 from django.template import Node
 
+"""
+http://docs.djangoproject.com/en/dev/howto/custom-template-tags/
+
+before rendering the template, include tags in the handler with
+    webapp.template.register_template_library('common.templateaddons')
+"""
+
 # get registry, we need it to register our filter later.
 register = webapp.template.create_template_register()
 
 
-# template-tag: use with {{ somevar|half }}
-# in the handler, before rendering the template, include tags with
-#    webapp.template.register_template_library('common.templateaddons')
-def half(n):
-    if not type(n) in [int, long]:
-        return n
-    return n / 2
+def getItem(l, id):
+    """
+    use template-tag with {{ my_tuple|getItem:0 }}
+    workaround because appengine templates can't do {% for x,y in sometuple %}
+    """
+    if type(l) == list and len(l) > id:
+        return l[id]
+    if type(l) == dict and id in l:
+        return l[id]
 
 register.filter(half)
