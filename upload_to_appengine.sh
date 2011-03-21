345aec2e475c8b0a30994e36b0f79773a6110065
Chris Hager <chager@myyearbook.com>
2011-03-21 18:32:24 -0400

    script for uploading to appengine (initiates the build process, changes /static link to build output, uploads and reverts /static to dev env

---
+++ upload_to_appengine.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DIR=$( pwd )
+
+function static_revert {
+  # Set /static back to dev environment
+  set -e
+  echo "Setting /static back to /static_dev"
+  cd app
+  rm static
+  ln -s static_dev static
+  cd "$DIR"
+  set +e
+}
+
+function static_toprod {
+  # Set build script results as /static
+  set -e
+  echo "Setting /static to /static_dev/publish"
+  cd app
+  rm static
+  ln -s static_dev/publish static
+  cd "$DIR"
+  set +e
+}
+
+function upload {
+  # Upload to appengine
+  ~/Tools/google_appengine/appcfg.py update app
+}
+
+function build {
+  cd app/static_dev/build
+  ant minify
+  cd "$DIR"
+}
+
+read -p "Build the project with 'ant minify' now? [yN]" yn
+  case $yn in
+    [Yy]* ) build;;
+    [Nn]* ) break;;
+esac
+
+static_toprod
+
+read -p "You can now test the latest build. Do you wish to upload this version? [yN]" yn
+  case $yn in
+    [Yy]* ) upload;;
+    [Nn]* ) break;;
+esac
+
+static_revert
+
+
