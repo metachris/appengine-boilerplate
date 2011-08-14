4e1ed6cccfede774023a9a0fe68ae70b8e5260ce
Chris Hager <chris@metachris.org>
2011-08-14 16:23:15 +0200

    put BaseRequestHandler into handlers/ directory for easier input from several handler files

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -4,50 +4,15 @@ import logging
 
 from hashlib import md5
 from google.appengine.ext import db
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 # Import packages from the project
 import mc
 from models import *
+from baserequesthandler import *
 from tools import *
 from decorators import *
-
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../templates/')
-
-
-class BaseRequestHandler(webapp.RequestHandler):
-    """Extension of the normal RequestHandler
-
-    - self.userprefs provides the UserPrefs object of the current user.
-    - self.render() provides a quick way to render templates with
-      common template variables already preset.
-    """
-    def __init__(self):
-        super(BaseRequestHandler, self).__init__()
-        self.userprefs = UserPrefs.from_user(users.get_current_user())
-
-    def render(self, template_name, template_values={}):
-        # Preset values for the template
-        values = {
-          'request': self.request,
-          'prefs': self.userprefs,
-          'login_url': users.create_login_url(self.request.uri),
-          'logout_url': users.create_logout_url(self.request.uri),
-        }
-
-        # Add manually supplied template values
-        values.update(template_values)
-
-        # Render template
-        fn = os.path.join(TEMPLATE_DIR, template_name)
-        self.response.out.write( \
-                template.render(fn, values, debug=is_testenv()))
-
-    def head(self, *args):
-        """Head is used by Twitter. If not there the tweet button shows 0"""
-        pass
 
 
 # OpenID login
